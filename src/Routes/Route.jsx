091b15dc06690e1e71f6f3e496bfadca09edf759
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-07-18 04:03:13 +0600

    mony request and cash out is implement is done

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Login from "../components/Pages/Login/Login";
 import Register from "../components/Pages/Register/Register";
-import PrivateRoute from '../Routes/PrivateRoute'
+import PrivateRoute from "../Routes/PrivateRoute";
 import Overview from "../components/Overview/Overview";
 import Transactions from "../components/Pages/Transactions/Transactions";
 import SendMoney from "../components/SendMoney/SendMoney";
@@ -11,43 +11,67 @@ import CashIn from "../components/CashIn/CashIn";
 import Transaction_Management from "../components/Pages/Transaction_Management/Transaction_Management";
 
 const route = createBrowserRouter([
-    {
-        path:'/',
-        element:<Layout></Layout>,
-        children:[
-        {
-            path:'/',
-            element:<PrivateRoute><Overview></Overview></PrivateRoute>
-        },
-        {
-            path:'/Transactions',
-            element:<PrivateRoute><Transactions></Transactions></PrivateRoute>
-        },
-        {
-            path:'/SendMoney',
-            element:<PrivateRoute><SendMoney></SendMoney></PrivateRoute>
-        },
-        {
-            path:'/CashOut',
-            element:<PrivateRoute><CashOut></CashOut></PrivateRoute>
-        },
-        {
-            path:'/CashIn',
-            element:<PrivateRoute><CashIn></CashIn></PrivateRoute>
-        },
-        {
-            path:'/Transaction_Management',
-            element:<PrivateRoute><Transaction_Management></Transaction_Management></PrivateRoute>
-        },
-        {
-            path:'/Login',
-            element:<Login></Login>
-        },
-        {
-            path:'/Register',
-            element:<Register></Register>
-        }
-    ]
-    }
-])
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: (
+          <PrivateRoute>
+            <Overview></Overview>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/Transactions",
+        element: (
+          <PrivateRoute>
+            <Transactions></Transactions>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/SendMoney",
+        element: (
+          <PrivateRoute>
+            <SendMoney></SendMoney>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/CashOut",
+        element: (
+          <PrivateRoute>
+            <CashOut></CashOut>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/CashIn",
+        element: (
+          <PrivateRoute>
+            <CashIn></CashIn>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/Transaction_Management",
+        element: (
+          <PrivateRoute>
+            <Transaction_Management></Transaction_Management>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/Login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/Register",
+    element: <Register></Register>,
+  },
+]);
 export default route;
--- src/components/Pages/Transaction_Management/Transaction_Management.jsx
+++ src/components/Pages/Transaction_Management/Transaction_Management.jsx
@@ -1,14 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const Transaction_Management = () => {
   const { user } = useAuth();
-  // console.log(user);
-
   const axiosPublic = useAxiosPublic();
 
-  const { data: Management = [] } = useQuery({
+  const { data: Management = [], refetch } = useQuery({
     queryKey: ["Transaction-Management"],
     queryFn: async () => {
       const res = await axiosPublic.get(
@@ -17,12 +16,61 @@ const Transaction_Management = () => {
       return res.data;
     },
   });
-  console.log(Management);
+  const handleCashed = async (M) => {
+    try {
+      const { data } = await axiosPublic.patch("/requested-cash-out", M);
+      if (data.message === "Cash Out Approve Successfully") {
+        Swal.fire({
+          position: "top",
+          icon: "success",
+          title: data?.message,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        refetch();
+      }
+    } catch (error) {
+      Swal.fire({
+        position: "top",
+        icon: "warning",
+        title: error?.response?.data?.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
+  const handleRequested = async (M) => {
+    try {
+      const { data } = await axiosPublic.patch("/requested-cash-in", M);
+      if (data.message === "Cash In Request Approve Successful") {
+        Swal.fire({
+          position: "top",
+          icon: "success",
+          title: data?.message,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        refetch();
+      }
+    } catch (error) {
+      Swal.fire({
+        position: "top",
+        icon: "warning",
+        title: error?.response?.data?.message,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
   return (
     <div>
-      <h1>This is Transaction Management page</h1>
+      <h1 className="text-center text-2xl font-semibold my-5">
+        Transaction Management
+      </h1>
       <div className="overflow-x-auto">
-        <table className="table table-zebra">
+        <table className="table">
           {/* head */}
           <thead>
             <tr>
@@ -35,17 +83,38 @@ const Transaction_Management = () => {
             </tr>
           </thead>
           <tbody>
-            {
-            Management.map((M,idx)=><tr key={idx}>
-            <th>{idx+1}</th>
-            <td>{M?.Name}</td>
-            <td>{M?.Email}</td>
-            <td>{M?.Phone}</td>
-            <td>{M?.Cash_Outed_Amount}</td>
-            <td>{M?.Requested_Amount}</td>
-          </tr>)
-            }
-            
+            {Management.map((M, idx) => (
+              <tr key={idx}>
+                <th>{idx + 1}</th>
+                <td>{M?.Name}</td>
+                <td>{M?.Email}</td>
+                <td>{M?.Phone}</td>
+                <td>
+                  {M?.Cash_Outed_Amount && (
+                    <button
+                      title="Please Approve Cashed Amount"
+                      onClick={() => handleCashed(M)}
+                      className="btn"
+                    >
+                      <span>Approve</span>
+                      {M?.Cash_Outed_Amount}
+                    </button>
+                  )}
+                </td>
+                <td>
+                  {M?.Requested_Amount && (
+                    <button
+                      title="Please Approve Requested Amount"
+                      onClick={() => handleRequested(M)}
+                      className="btn"
+                    >
+                      <span>Approve</span>
+                      {M?.Requested_Amount}
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
